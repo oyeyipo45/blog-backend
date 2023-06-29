3d5326926fbcbaacd8ca8888cdd6eb910d7eac97
Damilola Oyeyipo <oyeyipo45@gmail.com>
2023-06-29 13:25:52 +0100

    Create post dto

---
+++ src/main/java/com/blog/backend/dtos/PostDto.java
@@ -0,0 +1,12 @@
+package com.blog.backend.dtos;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
